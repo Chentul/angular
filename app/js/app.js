dcd77b53c7b61d229449be306228c9380e966711
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-22 11:13:37 -0500

    9. Configuración de TypeScript

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
-function saludar(nombre) { console.log("Hola " + nombre); }
+"use strict";
+function saludar(nombre) { console.log("Hola " + nombre.toUpperCase()); }
 var wolverine = { nombre: "Logan" };
 saludar(wolverine.nombre);
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,4 +1,4 @@
 
-function saludar( nombre:string ) { console.log( "Hola " + nombre ); }
+function saludar( nombre:string ) { console.log( "Hola " + nombre.toUpperCase() ); }
 var wolverine = { nombre: "Logan" };
 saludar( wolverine.nombre );
