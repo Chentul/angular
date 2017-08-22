64ef281065608c6ad6fc9d532b05278b96504972
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-22 18:58:05 -0500

    17. Promesas en ES6

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,15 @@
 "use strict";
-/*===DESTRUCTURACIÓN DE LOS OBJETOS
-==============================================================================*/
-var avenger = {
-    nombre: "Steve",
-    clave: "Capitan America",
-    poder: "Droga"
-};
-// let nombre = avenger.nombre;
-// let clave = avenger.clave;
-// let poder = avenger.poder;
-// let { nombre, clave, poder } = avenger;
-// let descripcion = `Descripción: ${ clave }\nNombre: ${ nombre }\nPoder: ${ poder }`;
-var supername = avenger.nombre, superheroe = avenger.clave, superpower = avenger.poder;
-var descripcion = "Descripci\u00F3n: " + superheroe + "\nNombre: " + supername + "\nPoder: " + superpower;
-console.log(descripcion);
-/*===DESTRUCTURACIÓN DE LOS ARREGLOS
-==============================================================================*/
-var avengers = ["Bruce Banner", "Steve Rogers", "Tony Stark"];
-var hulk = avengers[0], capitanamerica = avengers[1], ironman = avengers[2];
-// let [ , , ironman  ] = avengers; // indice del arreglo
-var texto2 = "Hulk: " + hulk + "\nCapitan America: " + capitanamerica + "\nIron Man: " + ironman;
-console.log(texto2);
+/* resolve = si la función se resuelve con exito,
+   reject = si la función es rechazada */
+var prom1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Promesa terminada ...");
+        resolve(); // si la promesa termina con exito
+        // reject(); // si la promesa es rechazada
+    }, 500);
+});
+prom1.then(function () {
+    console.log("Ejecutarme cuando la promesa termine correctamente! =)");
+}, function () {
+    console.error("Ejecutarme cuando la promesa termine rechazada! x(");
+});
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,30 +1,15 @@
-/*===DESTRUCTURACIÓN DE LOS OBJETOS
-==============================================================================*/
-let avenger = {
-  nombre: "Steve",
-  clave: "Capitan America",
-  poder: "Droga"
-};
+/* resolve = si la función se resuelve con exito,
+   reject = si la función es rechazada */
+let prom1 = new Promise( function( resolve, reject ) {
+  setTimeout( ()=> {
+    console.log( "Promesa terminada ..." );
+    resolve(); // si la promesa termina con exito
+    // reject(); // si la promesa es rechazada
+  }, 500 );
+});
 
-// let nombre = avenger.nombre;
-// let clave = avenger.clave;
-// let poder = avenger.poder;
-
-// let { nombre, clave, poder } = avenger;
-// let descripcion = `Descripción: ${ clave }\nNombre: ${ nombre }\nPoder: ${ poder }`;
-
-let { nombre:supername, clave:superheroe, poder:superpower } = avenger;
-let descripcion = `Descripción: ${ superheroe }\nNombre: ${ supername }\nPoder: ${ superpower }`;
-
-console.log( descripcion );
-
-/*===DESTRUCTURACIÓN DE LOS ARREGLOS
-==============================================================================*/
-let avengers:string[] = [ "Bruce Banner", "Steve Rogers", "Tony Stark" ];
-let [ hulk, capitanamerica, ironman  ] = avengers;
-// let [ , , ironman  ] = avengers; // indice del arreglo
-let texto2 =
-`Hulk: ${ hulk }
-Capitan America: ${ capitanamerica }
-Iron Man: ${ ironman }`;
-console.log( texto2 );
+prom1.then( function() {
+  console.log( "Ejecutarme cuando la promesa termine correctamente! =)" );
+},          function() {
+  console.error( "Ejecutarme cuando la promesa termine rechazada! x(" );
+});
