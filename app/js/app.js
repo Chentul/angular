3b3a83e63b62bf00aa8eb754babe206deaf7b64f
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-22 18:23:24 -0500

    15. Funciones de Flecha

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,35 @@
 "use strict";
-function activar(quien, objeto, momento) {
-    if (objeto === void 0) { objeto = "batiseñal"; }
-    var mensaje;
-    if (momento) {
-        mensaje = quien + " activ\u00F3 la " + objeto + " en la " + momento;
+var miFuncion = function (a) { return a; };
+var miFuncionF = function (a) { return a; };
+console.log(miFuncion("Función normal ..."));
+console.log(miFuncionF("Función con flecha ..."));
+console.log("\n");
+var miFuncion2 = function (a, b) { return a + b; };
+var miFuncion2F = function (a, b) { return a + b; };
+console.log("Función normal, suma: " + miFuncion2(3, 5));
+console.log("Función flecha, suma: " + miFuncion2F(3, 5));
+console.log("\n");
+var miFuncion3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var miFuncion3F = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+console.log("Función normal, nombre: " + miFuncion3("Vicente Spencer Noriega Moreno"));
+console.log("Función flecha, nombre: " + miFuncion3F("Vicente Spencer Noriega Moreno"));
+console.log("\n");
+/* Objetos en ECMA 6
+==============================================================================*/
+// let nombre = "Pedro";
+var hulk = {
+    nombre: "Bruce",
+    estado: "Furioso",
+    smash: function () {
+        var _this = this;
+        // setTimeout( function(){ console.log( this.nombre + " smash!!" ); }, 1500 );
+        setTimeout(function () { console.log(_this.nombre + " smash!!"); }, 1500);
     }
-    else {
-        mensaje = quien + " activ\u00F3 la " + objeto;
-    }
-    console.log(mensaje);
-}
-activar("El Guasón");
-activar("Doble Cara", "baticueba");
-activar("James Gordon", "batiseñal", "noche.");
+}; // fin del objeto hulk
+hulk.smash();
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,13 +1,39 @@
-function activar( quien:string,
-                  objeto:string = "batiseñal",
-                  momento?:string ) {
-  let mensaje:string;
-  if( momento ) { mensaje = `${ quien } activó la ${ objeto } en la ${ momento }`; }
-  else { mensaje = `${ quien } activó la ${ objeto }`; }
+let miFuncion = function( a:any ) { return a; }
+let miFuncionF = ( a:any ) => a;
 
-  console.log( mensaje );
+console.log( miFuncion( "Función normal ..." ) );
+console.log( miFuncionF( "Función con flecha ..." ) );
+console.log( "\n" );
+
+let miFuncion2 = function( a:number, b:number ) { return a + b; }
+let miFuncion2F = ( a:number, b:number ) => a + b;
+
+console.log( "Función normal, suma: " + miFuncion2( 3, 5 ) );
+console.log( "Función flecha, suma: " + miFuncion2F( 3, 5 ) );
+console.log( "\n" );
+
+let miFuncion3 = function( nombre:string ) {
+  nombre = nombre.toUpperCase();
+  return nombre;
+}
+let miFuncion3F = ( nombre:string ) => {
+  nombre = nombre.toUpperCase();
+  return nombre;
 }
 
-activar( "El Guasón" );
-activar( "Doble Cara", "baticueba" );
-activar( "James Gordon", "batiseñal", "noche." );
+console.log( "Función normal, nombre: " + miFuncion3( "Vicente Spencer Noriega Moreno" ) );
+console.log( "Función flecha, nombre: " + miFuncion3F( "Vicente Spencer Noriega Moreno" ) );
+console.log( "\n" );
+/* Objetos en ECMA 6
+==============================================================================*/
+// let nombre = "Pedro";
+let hulk = {
+  nombre: "Bruce",
+  estado: "Furioso",
+  smash() {
+    // setTimeout( function(){ console.log( this.nombre + " smash!!" ); }, 1500 );
+    setTimeout( () => { console.log( this.nombre + " smash!!" ); }, 1500 );
+  }
+}; // fin del objeto hulk
+
+hulk.smash();
