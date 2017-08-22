514e9de0e6b8122fa62be21861318c7d2415305e
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-22 13:47:28 -0500

    13. Template literales del ES6

--- app/js/app.js
+++ app/js/app.js
@@ -1,65 +1,13 @@
 "use strict";
-/*=== Tipos de datos en TypeScript
-==============================================================================*/
-console.log("\n");
-st();
-var cadenaTexto = "Vicente Spencer Noriega Moreno";
-console.log("Tipo de dato STRING");
-console.log(cadenaTexto);
-bk();
-st();
-var numero = 123.25;
-console.log("Tipo de dato NUMBER");
-console.log(numero);
-bk();
-st();
-var boleano = true;
-console.log("Tipo de dato BOOLEAN");
-console.log(boleano);
-bk();
-st();
-var fecha = new Date();
-fecha = new Date("2020-10-21");
-console.log("Objeto de tipo DATE");
-console.log(fecha);
-bk();
-st();
-var any;
-console.log("Tipo de dato ANY");
-any = cadenaTexto;
-console.log(any);
-any = numero;
-console.log(any);
-any = boleano;
-console.log(any);
-any = fecha;
-console.log(any);
-bk();
-st();
-var spiderman = {
-    nombre: "Peter Parker",
-    edad: 26
-};
-console.log("Tipo de dato Objeto");
-console.log(spiderman.nombre);
-console.log(spiderman.edad);
-bk();
-/*=== Funciones
-==============================================================================*/
-function bk() {
-    var char = "*", count = 1;
-    while (count <= 50) {
-        char += "*";
-        count++;
-    }
-    console.log(char);
-    console.log("\n");
+var nombre = "Vicente Spencer";
+var apellido = "Noriega Moreno";
+function edad() {
+    var edad = 2017 - 1991;
+    return edad;
 }
-function st() {
-    var char = "*", count = 1;
-    while (count <= 50) {
-        char += "*";
-        count++;
-    }
-    console.log(char);
-}
+// let texto = "Hola, " + nombre + " " + apellido + ", (" + edad + " años)";
+// let texto = `Hola, ${ nombre } ${ apellido }, (${ edad } años)`;
+var texto = "Nombre " + nombre + " " + apellido + "\nEdad: " + edad() + " a\u00F1os";
+console.log("***************************************************");
+console.log(texto);
+console.log("***************************************************\n");
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,58 +1,14 @@
-/*=== Tipos de datos en TypeScript
-==============================================================================*/
-console.log( "\n" );
-st();
-let cadenaTexto:string = "Vicente Spencer Noriega Moreno";
-console.log( "Tipo de dato STRING" );
-console.log( cadenaTexto );
-bk();
-
-st();
-let numero:number = 123.25;
-console.log( "Tipo de dato NUMBER" );
-console.log( numero );
-bk();
-
-st();
-let boleano:boolean = true;
-console.log( "Tipo de dato BOOLEAN" );
-console.log( boleano );
-bk();
-
-st();
-let fecha:Date = new Date();
-fecha = new Date( "2020-10-21" );
-console.log( "Objeto de tipo DATE" );
-console.log( fecha );
-bk();
-
-st();
-let any:any;
-console.log( "Tipo de dato ANY" );
-any = cadenaTexto; console.log( any );
-any = numero; console.log( any );
-any = boleano; console.log( any );
-any = fecha; console.log( any );
-bk();
-
-st();
-let spiderman = {
-  nombre: "Peter Parker",
-  edad: 26
-};
-console.log( "Tipo de dato Objeto" );
-console.log( spiderman.nombre );
-console.log( spiderman.edad );
-bk();
-/*=== Funciones
-==============================================================================*/
-function bk() {
-  let char:string = "*", count:number = 1;
-  while( count <= 50 ) { char += "*"; count++; }
-  console.log( char ); console.log( "\n" );
+let nombre:string     = "Vicente Spencer";
+let apellido:string   = "Noriega Moreno";
+function edad() {
+  let edad:number     = 2017 - 1991;
+  return edad;
 }
-function st() {
-  let char:string = "*", count:number = 1;
-  while( count <= 50 ) { char += "*"; count++; }
-  console.log( char );
-}
+
+// let texto = "Hola, " + nombre + " " + apellido + ", (" + edad + " años)";
+// let texto = `Hola, ${ nombre } ${ apellido }, (${ edad } años)`;
+let texto = `Nombre ${ nombre } ${ apellido }
+Edad: ${ edad() } años`;
+console.log( "***************************************************" );
+console.log( texto );
+console.log( "***************************************************\n" );
