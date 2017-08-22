5b5ddab52393b3a42fbf1f20a9e950df07342ef0
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-22 12:38:06 -0500

    10. Variables let y const

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,15 @@
 "use strict";
-function saludar(nombre) { console.log("Hola " + nombre.toUpperCase()); }
-var wolverine = { nombre: "Logan" };
-saludar(wolverine.nombre);
+/* La diferencia es el alcance de las variables. let permite declarar variables limitando su alcance al bloque, declaración, o expresión donde se está usando y var define una variable global o local en una función sin importar el ámbito del bloque. */
+var message = "Hello World!";
+console.log("Scrope 1: " + message);
+if (true) {
+    var message_1 = "Good Bye!";
+    console.log("Scope 2: " + message_1);
+}
+/* Uso de constantes en TypeScript */
+var OPCION = "Esto es una constante en TypeScript ...";
+console.log("Scope 3: " + OPCION);
+if (true) {
+    var OPCION_1 = "Esta es la misma constante en otro bloque de TypeScript ...";
+    console.log("Scope 4: " + OPCION_1);
+}
--- app/js/app.ts
+++ app/js/app.ts
@@ -1,4 +1,17 @@
+/* La diferencia es el alcance de las variables. let permite declarar variables limitando su alcance al bloque, declaración, o expresión donde se está usando y var define una variable global o local en una función sin importar el ámbito del bloque. */
+let message = "Hello World!";
+console.log( "Scrope 1: " + message );
 
-function saludar( nombre:string ) { console.log( "Hola " + nombre.toUpperCase() ); }
-var wolverine = { nombre: "Logan" };
-saludar( wolverine.nombre );
+if( true ) {
+  let message = "Good Bye!";
+  console.log( "Scope 2: " + message );
+}
+
+/* Uso de constantes en TypeScript */
+const OPCION = "Esto es una constante en TypeScript ...";
+console.log( "Scope 3: " + OPCION );
+
+if( true ) {
+  const OPCION = "Esta es la misma constante en otro bloque de TypeScript ...";
+  console.log( "Scope 4: " + OPCION );
+}
