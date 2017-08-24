f3164575fa06e8766f67386c0de870ef34578b73
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-24 16:58:32 -0500

    35. TemplateUrl: Separando el template del componenete

--- 1. Hola Mundo/src/app/components/header.component.ts
+++ 1. Hola Mundo/src/app/components/header.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>Este es el header component</h1>`
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent { }
