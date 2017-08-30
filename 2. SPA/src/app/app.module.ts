465d805663a200483ecf210cc77707bc36cde4c0
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-30 13:17:43 -0500

    44. Creando la estructura de nuestro proyecto

--- 2. SPA/src/app/app.module.ts
+++ 2. SPA/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// se agrego el componente navbar
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent // se agrego la declaración del componente navbar
   ],
   imports: [
     BrowserModule
