d8ec72c7601ad4d06bd82d94d5c38952ff8dce21
Vicente Spencer Noriega Moreno <web@coleccionbelair.com>
2017-08-30 18:32:00 -0500

    46. Configurando el navbar y otros componentes

--- 2. SPA/src/app/app.module.ts
+++ 2. SPA/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 // se agrego el componente navbar
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent // se agrego la declaración del componente navbar
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent // se agrego la declaración del componente navbar
   ],
   imports: [
     BrowserModule
